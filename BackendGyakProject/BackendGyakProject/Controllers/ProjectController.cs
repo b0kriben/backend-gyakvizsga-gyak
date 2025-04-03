513e61159a7c072c1029481cb4eb430d6dd773d5
Krisztin Benedek <krisztinbeni@gmail.com>
2025-04-03 15:26:51 +0200

    folyamatban

---
+++ BackendGyakProject/BackendGyakProject/Controllers/ProjectController.cs
@@ -0,0 +1,16 @@
+﻿using BackendGyakProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendGyakProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly DatabaseContext _orderContext = new DatabaseContext();
+
+
+    }
+}
